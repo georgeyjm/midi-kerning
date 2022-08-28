54d222f1c8a4a2f8eba426ff9cf9ada53e2a9665
George Yu <yudumpling@gmail.com>
2022-08-28 12:11:27 +0800

    Boilerplate code

---
+++ ____PluginName____.glyphsPlugin/Contents/Resources/plugin.py
@@ -0,0 +1,45 @@
+# encoding: utf-8
+
+###########################################################################################################
+#
+#
+#	General Plugin
+#
+#	Read the docs:
+#	https://github.com/schriftgestalt/GlyphsSDK/tree/master/Python%20Templates/General%20Plugin
+#
+#
+###########################################################################################################
+
+from __future__ import division, print_function, unicode_literals
+import objc
+from GlyphsApp import *
+from GlyphsApp.plugins import *
+
+class MidiKerning(GeneralPlugin):
+
+	@objc.python_method
+	def settings(self):
+		self.name = 'MIDI Kerning'
+		# self.name = Glyphs.localize({
+		# 	'en': 'My General Plug-in',
+		# 	'de': 'Mein allgemeines Plug-in',
+		# 	'fr': 'Ma extension générale',
+		# 	'es': 'Mi plugin general',
+		# 	'pt': 'Meu plug-in geral',
+		# 	})
+
+	@objc.python_method
+	def start(self):
+		newMenuItem = NSMenuItem(self.name, self.showWindow_)
+		Glyphs.menu[GLYPH_MENU].append(newMenuItem)
+
+	def showWindow_(self, sender):
+		"""Do something like show a window """
+		# print("show Windows")
+		print()
+
+	@objc.python_method
+	def __file__(self):
+		"""Please leave this method unchanged"""
+		return __file__
