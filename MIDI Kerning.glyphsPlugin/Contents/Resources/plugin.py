f8809749853244879d54a748df647d24f810422c
George Yu <yudumpling@gmail.com>
2022-08-29 14:52:28 +0800

    Complete first MVP

--- MIDI Kerning.glyphsPlugin/Contents/Resources/plugin.py
+++ MIDI Kerning.glyphsPlugin/Contents/Resources/plugin.py
@@ -1,7 +1,16 @@
-from time import sleep
+import sys
+import time
 from threading import Thread
 
+# Temporary fix
+sys.path.append('/Users/george/miniforge3/envs/3type/lib/python3.9/site-packages')
+# Maybe?
+# import pip
+# pip.main(['install', 'mido', '--user'])
+# pip.main(['install', 'python-rtmidi', '--user'])
+
 import objc
+import mido
 from GlyphsApp import *
 from GlyphsApp.plugins import *
 
@@ -25,23 +34,34 @@ class MidiKerning(GeneralPlugin):
 
     @objc.python_method
     def start(self):
+        self.device_name = 'MPK mini 3'
+        assert self.device_name in mido.get_input_names()
+        self.cc = 23
+
         self.thread = Thread(target=self.listenThread)
         self.thread.daemon = True
         self.thread.start()
+
+        # Glyphs.showNotification('Test', str(sys.path[::-1]))
+
+        print('Started')
     
     
     def listenThread(self):
-        while True:
-            sleep(3)
-
-            for i in range(20):
-                self.updateKerning_(1)
-                sleep(0.02)
+        sign = lambda n: 1 if n > 0 else (-1 if n < 0 else 0)
+        with mido.open_input(self.device_name) as inport:
+            for msg in inport:
+                if msg.type != 'control_change':
+                    continue
+                if msg.control != self.cc:
+                    continue
+                self.updateKerning_(sign(64 - msg.value))
 
     
     def getAdjacentGlyphs(self, direction='right'):
         '''Returns either the previous and current glyphs,
         or the current and next glyphs, depending on the direction.'''
+
         assert direction in ('left', 'right')
         increment = 1 if direction == 'right' else -1
 
@@ -63,18 +83,24 @@ class MidiKerning(GeneralPlugin):
 
 
     def updateKerning_(self, diff):
+        start1 = time.time()
         font = Glyphs.font
         if len(font.selectedLayers) != 1:
             return
+        end1 = time.time()
 
+        start2 = time.time()
         active_glyph, next_glyph = self.getAdjacentGlyphs()
+        end2 = time.time()
+        start3 = time.time()
         curr_kerning = font.kerningForPair(font.selectedFontMaster.id, active_glyph.name, next_glyph.name)
+        end3 = time.time()
         if curr_kerning is None:
             curr_kerning = 0
-        new_kerning = curr_kerning + diff
-
-        # Glyphs.showNotification('Test', active_glyph.name + next_glyph.name + str(curr_kerning)) # .string, .id, .name
-        
+        start4 = time.time()
         # active_glyph.beginUndo()
-        font.setKerningForPair(font.selectedFontMaster.id, active_glyph.name, next_glyph.name, new_kerning)
+        font.setKerningForPair(font.selectedFontMaster.id, active_glyph.name, next_glyph.name, curr_kerning + diff)
         # active_glyph.endUndo()
+        end4 = time.time()
+
+        Glyphs.showNotification('Test', f'{end1-start1}, {end2-start2}, {end3-start3}, {end4-start4}') # .string, .id, .name
